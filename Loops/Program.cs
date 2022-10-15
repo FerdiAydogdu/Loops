a13f171df7edec295b1538d0ed57e543e76b2a2f
ferdi <aydogduferdi14@gmail.com>
2022-10-15 22:39:07 +0300

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun()
+            {
+                Id = 1,
+                UrunAdi = "Masa",
+                Fiyat = 750
+            };
+
+            Urun urun2 = new Urun()
+            {
+                Id = 2,
+                UrunAdi = "Bilgisayar",
+                Fiyat = 8400
+            };
+
+            Urun urun3 = new Urun()
+            {
+                Id = 3,
+                UrunAdi = "Televizyon",
+                Fiyat = 6200
+            };
+
+            Urun[] urunler = new Urun[] { urun1, urun2, urun3 };
+
+            for (int i = 0; i < urunler.Length; i++)
+            {
+                Console.WriteLine(urunler[i].Id + " : " + urunler[i].UrunAdi + " : " + urunler[i].Fiyat);
+            }
+
+            Console.WriteLine("---------------------");
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Id + " : " + urun.UrunAdi + " : " + urun.Fiyat);
+            }
+
+            Console.WriteLine("---------------------");
+
+
+            int j = 0;
+            while (j < urunler.Length)
+            {
+                Console.WriteLine(urunler[j].Id + " : " + urunler[j].UrunAdi + " : " + urunler[j].Fiyat);
+                j++;
+            }
+        }
+    }
+}
---
+++ Loops/Urun.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loops
+{
+    class Urun
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public decimal Fiyat { get; set; }
+    }
+}
